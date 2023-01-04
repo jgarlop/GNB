dff37bb884edff9d6221fdec656e79df441f16d6
Javier García López <jgarcial@wabiproject.com>
2023-01-04 12:09:16 +0100

    Initial Commit

---
+++ GNB/GNBApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNBApp.swift
+//  GNB
+//
+//  Created by Javier Garcia on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GNBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
