7a278889233a074876d0e20f990bcff48db588cb
Javier García López <jgarcial@wabiproject.com>
2023-01-09 00:14:41 +0100

    feat(ProductDetail): add data and domain models

---
+++ GNB/Modules/ProductDetail/Domain/Entities/ExchangeRate.swift
@@ -0,0 +1,14 @@
+//
+//  ExchangeRate.swift
+//  GNB
+//
+//  Created by Javier Garcia on 8/1/23.
+//
+
+import Foundation
+
+struct ExchangeRate {
+    let origin: String
+    let destination: String
+    let rate: Decimal
+}
